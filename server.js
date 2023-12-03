4e7497bf32149e4957a744c3f4c6b27bbbac0f88
Aayush Tiwari <tiwariaayush682@gmail.com>
2023-12-03 16:34:52 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const ejs = require("ejs");
+const ejsLayouts = require("express-ejs-layouts");
+const webRoutes = require("./routes/web");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use(webRoutes);
+
+// set view engine
+app.set("view engine", "ejs");
+
+//set template engine
+app.use(ejsLayouts);
+app.set('views', path.join(__dirname, '/resources/views'))
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+})
