85656ea4ceac1def89722fd6cb3817db9f611157
Aayush Tiwari <tiwariaayush682@gmail.com>
2023-12-07 14:45:29 +0530

    Add Auth Page

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
 const ejsLayouts = require("express-ejs-layouts");
-const webRoutes = require("./routes/web");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,16 +9,31 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(ejsLayouts);
 
-//routes
-app.use(webRoutes);
+//set template engine
+app.set('views', path.join(__dirname, '/resources/views'))
 
 // set view engine
 app.set("view engine", "ejs");
 
-//set template engine
-app.use(ejsLayouts);
-app.set('views', path.join(__dirname, '/resources/views'))
+//routes
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart")
+})
+
+app.get("/register", (req, res) => {
+  res.render("auth/register")
+})
+
+app.get("/login", (req, res) => {
+  res.render("auth/login")
+})
+
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
